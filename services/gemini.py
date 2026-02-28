7cb9ed8eed8c98d6b0a5e756f9306a89c1b769d0
Saadetatabay <etatabaysaad143@gmail.com>
2026-02-28 23:44:11 +0300

    Move generativeai configuration inside get_outfit_advice function

--- services/gemini.py
+++ services/gemini.py
@@ -4,10 +4,10 @@ from google import generativeai
 
 load_dotenv()
 api_key = os.getenv("API_KEY_GEMINI")
-generativeai.configure(api_key=api_key)
 model = generativeai.GenerativeModel("gemini-2.5-flash")
 
 def get_outfit_advice(temp,description):
+    generativeai.configure(api_key=api_key)
     prompt="Given the temperature in Celsius and the weather description, provide outfit advice. Temperature: {}°C, Weather: {}".format(temp,description)
     response=model.generate_content(prompt)
     return response.text

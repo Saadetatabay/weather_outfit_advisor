0437053a3a79d0d8f568ea68d52489dd78811db6
Saadetatabay <etatabaysaad143@gmail.com>
2026-02-28 23:49:33 +0300

    Refactor gemini service to use updated genai client and streamline outfit advice generation

--- services/gemini.py
+++ services/gemini.py
@@ -1,14 +1,11 @@
 from dotenv import load_dotenv
 import os
-from google import generativeai
+from google import genai
 
-load_dotenv()
-api_key = os.getenv("API_KEY_GEMINI")
-model = generativeai.GenerativeModel("gemini-2.5-flash")
 
 def get_outfit_advice(temp,description):
-    generativeai.configure(api_key=api_key)
+    client = genai.Client(api_key=os.getenv("API_KEY_GEMINI"))
     prompt="Given the temperature in Celsius and the weather description, provide outfit advice. Temperature: {}°C, Weather: {}".format(temp,description)
-    response=model.generate_content(prompt)
+    response=client.models.generate_content(model="gemini-2.5-flash",contents=prompt)
     return response.text
 

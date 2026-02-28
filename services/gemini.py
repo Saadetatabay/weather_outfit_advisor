5499f3c226aa5b18f414d74803f9d9bf0925874a
Saadetatabay <etatabaysaad143@gmail.com>
2026-02-28 21:29:20 +0300

    Implement outfit advice generation based on temperature and weather description

--- services/gemini.py
+++ services/gemini.py
@@ -7,3 +7,8 @@ api_key = os.getenv("API_KEY_GEMINI")
 generativeai.configure(api_key=api_key)
 model = generativeai.GenerativeModel("gemini-1.5-flash-002")
 
+def get_outfit_advice(temp,description):
+    prompt="Given the temperature in Celsius and the weather description, provide outfit advice. Temperature: {}°C, Weather: {}".format(temp,description)
+    response=model.generate_content(prompt)
+    return response.text
+

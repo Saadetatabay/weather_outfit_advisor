2abafe83e259c08217588386ad3148c6793ea4b2
Saadetatabay <etatabaysaad143@gmail.com>
2026-02-28 23:16:20 +0300

    Refactor main application to serve HTML index page and add static index.html file

--- main.py
+++ main.py
@@ -2,10 +2,17 @@ from fastapi import FastAPI
 from models import WeatherRequest,WeatherResponse
 from services.gemini import get_outfit_advice
 from services.weather import get_weather
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
-@app.post("/")
+@app.get("/", response_class=HTMLResponse)
+def index():
+    with open("static/index.html") as f:
+        return f.read()
+
+
+@app.post("/advice")
 def root(request:WeatherRequest):
     cityname = request.city
     temp,desc=get_weather(cityname)
